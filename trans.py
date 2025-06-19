0688149143352d105a5d9f37e860283e548c1a9e
Alexandre-Coulibaly <amk.coul@gmail.com>
2025-06-20 00:13:35 +0200

    Delete trans.py

--- trans.py
+++
@@ -1,6 +0,0 @@
-# Pseudo-code
-from transformers import AutoTokenizer, AutoModelForCausalLM, CLIPModel
-
-text_tokenizer = AutoTokenizer.from_pretrained("google/gemma-3b")
-text_model = AutoModelForCausalLM.from_pretrained("google/gemma-3b")
-vision_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
